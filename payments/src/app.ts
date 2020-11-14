b49bf4076af71709a8492c68f3c779668d11c6d4
jjayoma2017 <jjayoma2017@gmail.com>
2020-11-14 16:23:15 +0800

    started stripe feature

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -1,25 +1,25 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { createChargeRouter } from "./routes/new";
 
-
-import { errorHandler, NotFoundError, currentUser } from '@jtjticketing/common';
+import { errorHandler, NotFoundError, currentUser } from "@jtjticketing/common";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    secure: process.env.NODE_ENV !== "test",
   })
 );
 
 app.use(currentUser);
+app.use(createChargeRouter);
 
-
-app.all('*', async (req, res) => {
+app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
 
--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -1,22 +1,22 @@
-import { MongoMemoryServer } from 'mongodb-memory-server';
-import mongoose from 'mongoose';
-import { app } from '../app';
-import request from 'supertest';
-import jwt from 'jsonwebtoken';
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import { app } from "../app";
+import request from "supertest";
+import jwt from "jsonwebtoken";
 
 declare global {
   namespace NodeJS {
     interface Global {
-      signin(): string[];
+      signin(id?: string): string[];
     }
   }
 }
 
-jest.mock('../nats-wrapper');
+jest.mock("../nats-wrapper");
 
 let mongo: any;
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asdf';
+  process.env.JWT_KEY = "asdf";
   //process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
   mongo = new MongoMemoryServer();
@@ -42,11 +42,11 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = () => {
+global.signin = (id?: string) => {
   // Build a JWT payload. {id, email}
   const payload = {
-    id: mongoose.Types.ObjectId().toHexString(),
-    email: 'test@test.com',
+    id: id || mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
   };
   // Create the JWT!
   const token = jwt.sign(payload, process.env.JWT_KEY!);
@@ -55,7 +55,7 @@ global.signin = () => {
   // Turn that session into JSON
   const sessionJSON = JSON.stringify(session);
   // Take JSON and encode it as base64
-  const base64 = Buffer.from(sessionJSON).toString('base64');
+  const base64 = Buffer.from(sessionJSON).toString("base64");
   // return a string thats the cookie with the encoded data
   return [`express:sess=${base64}`];
 };
