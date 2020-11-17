ecc2fad6ff8e8a85fc34f91a65bcef9f0fc205a6
jjayoma2017 <jjayoma2017@gmail.com>
2020-11-17 13:14:37 +0800

    updated client pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,3 +1,4 @@
+// _app.js => custom app component. Default page. All global styling must be placed in here
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from '../api/build-client';
 import Header from '../components/header';
@@ -6,18 +7,31 @@ const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <div className='container'>
+        <Component currentUser={currentUser} {...pageProps} />
+      </div>
     </div>
   );
 };
 
+/**
+ * Server side rendering method. Call data from server. This method is Executed from theserver.
+ * @param  {object} context The standard single argument that getInitialProps received.
+ *                          This object contains these props: pathname,query,asPath,req,
+ *                          res,err
+ * @return {object} pageProps,data The values  returned from the currentuser api. The returned data will be
+ *                        automatically passed into the LandingPage method above
+ */
 AppComponent.getInitialProps = async (appContext) => {
-  const { data } = await buildClient(appContext.ctx).get(
-    '/api/users/currentuser'
-  );
+  const client = await buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
   let pageProps = {};
   if (appContext.Component.getInitialProps)
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx,
+      client,
+      data.currentUser
+    );
 
   return {
     pageProps,
